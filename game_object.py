cb9639e57cdbc8e9f8331fad0767f9ce9a548960
Brandon Phelps <bwp44f@mst.edu>
2015-02-22 09:18:36 -0600

    made it possible for accessing multiple elements of each properity rather than only indivdiual access

--- game_object.py
+++ game_object.py
@@ -15,6 +15,8 @@ class Game(object):
         self.created_units = 0
         self.total_turns = 0
         self.depth = 0
+        self.player1_created_units = 0
+        self.player2_created_units = 0
 
     def unit_created(self):
         self.created_units += 1
@@ -25,14 +27,10 @@ class Game(object):
         sorted_keys = data.keys()
         sorted_keys.sort()
         for i in sorted_keys:
-            print i
             self._process(data[i])
-
             
     def _process(self, data, key=None):
-        self.depth += 1
         for i in data:
-            print i
             if type(i) == type(list()):
                 self._process(list_data(i), list_key(i))
             else:
@@ -41,9 +39,10 @@ class Game(object):
                     j = []
                     # special case for player since the quotes were removed
                     if k == 'Player':
+                        k += '_order'
+                        k = k.lower()
                         temp = i.split(' ')
                         lenth = len(self.glogdata.get_funcs(k))
-                        print temp, lenth
                         c_l = 0
                         while c_l < lenth - 2:
                             j.append(temp[len(temp)-(c_l+1)])
@@ -51,30 +50,41 @@ class Game(object):
                         player_name = temp[1:len(temp)-2]
                         j.append(player_name)
                         j.append(temp[0])
+                        k = 'player'
                         j = j[::-1]
+                    elif k == 'animations':
+                        k = list_key(i)
+                        j = extracted_data(i)
                     else:
-                        j = i
+                        j = [i.split(' ')]
                 else:
                     k = list_key(i)
                     j = extracted_data(i)
 
+                k += '_func'
+                k = k.lower()
+
                 if self.glogdata.params().count(k):
-                    print "Processing", k, repr(j)
-                    specific_funs = self.glogdata.get_funcs(k)
-                    for t, s in zip(specific_funs, j):
-                        print t, s
-                        t(s)
+                    print "Processing", k, j                
+                    self.glogdata.get_funcs(k)(j)
+                    
+                    # specific_funs = self.glogdata.get_funcs(k)
+                    # for t, s in zip(specific_funs, j):
+                    #    print t, s
+                    #    t(s)
 
 class GameData(object):
     def __init__(self):
         self.Player = [self.empty_func, self.empty_func, self.empty_func]
+        self.animation = [self.empty_func, self.empty_func]
+        # self.add = [self.add_func]
 
     def set_game(self, gameObj):
         self.gameObj = gameObj
 
     def params(self):
         return self.__dict__.keys()
-    
+
     def get_funcs(self, spec):
         if self.params().count(spec):
             return self.__dict__[spec]
@@ -82,6 +92,9 @@ class GameData(object):
             return None
 
     def empty_func(self, data):
+        pass
+
+    def add_func(self, data):
         pass
 
     def width_func(self, data):
@@ -99,24 +112,76 @@ class GameData(object):
     def set_current_turn(self, data):
         self.current_turn = int(data)
 
+    def get_item(self, string, d_type, data):
+        return data[d_type.index(string)]
+
 class ChessData(GameData):
     def __init__(self):
         super(ChessData, self).__init__()
-        self.game = [self.empty_func, self.turn_func, self.empty_func, self.empty_func]
+        self.game_order = [self.empty_func, self.turn_func, self.empty_func, self.empty_func]
+    
+    def game_func(self, data):
+        print data
+        pass
         
 class PlantsData(GameData):
     def __init__(self):
         super(PlantsData, self).__init__()
-        self.game = [self.width_func, self.height_func,
-                     self.turn_func, self.empty_func, 
-                     self.empty_func, self.empty_func, 
-                     self.empty_func, self.empty_func, 
-                     self.empty_func, self.empty_func,
-                     self.empty_func, self.empty_func]
-        self.Player.append(self.spore_count)
+
+        self.game_func = self._game_func
+        self.player_func = self._player_func
+
+        self.game_order = ['width', 'height', 'turn', 'empty', 'empty']
+        self.player_order = ['player_number', 'player_name', 'time_left', 'spore_count']
+
+        self.plant_ids_found = []
+        self.player1_ids = []
+        self.player2_ids = []
+
+        # self.uproot = [self.empty_func, self.empty_func, self.empty_func, self.empty_func, self.empty_func]
+        # self.animations = [self.empty_func]
+
+    def _game_func(self, data):
+        turns = self.get_item('turn', self.game_order, data)
+        self.current_turn = int(turns)
+        if self.gameObj.total_turns < int(turns):
+            self.gameObj.total_turns = int(turns)
+
+        self.gameObj.width = int(self.get_item('width', self.game_order, data))
+        self.gameObj.height = int(self.get_item('height', self.game_order, data))
+
+    def _player_func(self, data):
+        s_d = self.get_item('spore_count', self.player_order, data)
+        if self.current_turn == 0:
+            self.gameObj.spore_count_by_turn = [float(s_d)]
+        else:
+            self.gameObj.spore_count_by_turn.append(float(s_d))
+
+
+    def plant_func(self, data):
+        # print "PLANT FUNC", data
+        pass
+
+    def plant_id(self, data):
+        if not self.plant_ids_found.count(int(data)):
+            self.plant_ids_found.append(int(data))
+        if self.gameObj.created_units < len(self.plant_ids_found):
+            self.gameObj.created_units = len(self.plant_ids_found)
+
+    def plant_owner(self, data):
+        d = int(data)
+        if d == 0:
+            if not self.player1_ids.count(d):
+                self.player1_ids.append(d)
+                if self.gameObj.player1_created_units < len(self.player1_ids):
+                    self.gameObj.player1_created_units = len(self.player1_ids)
+        elif d == 1:
+            if not self.player2_ids.count(d):
+                self.player2_ids.append(d)
+                if self.gameObj.player2_created_units < len(self.player2_ids):
+                    self.gameObj.player2_created_units = len(self.player2_ids)
 
     def spore_count(self, data):
-        print("Spore count")
         if self.current_turn == 0:
             self.gameObj.spore_count_by_turn = [float(data)]
         else:
